c648c6eee66e8e2da794264fcbe2b9c85168b574
Emilio <emiliogg84@outlook.com>
2024-10-23 10:12:00 -0500

    Create EntityNum.cs

---
+++ src/Sumodesk.Domain/Abstractions/EntityNum.cs
@@ -0,0 +1,8 @@
+﻿using Sumodesk.Domain.Interfaces;
+
+namespace Sumodesk.Domain.Abstractions;
+
+public abstract class EntityNum : Entity, IEntityId, IEntityNum
+{
+    public required string Num { get; set; }
+}
