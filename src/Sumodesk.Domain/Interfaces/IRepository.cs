69688746766db19a40265a292a90d51d47eb1250
Emilio <emiliogg84@outlook.com>
2024-10-23 13:22:09 -0500

    Create IRepository.cs

---
+++ src/Sumodesk.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Sumodesk.Domain.Interfaces;
+
+public interface IRepository<TEntity> : IReadOnlyRespository<TEntity>
+	where TEntity : class, IEntityRoot
+{
+	bool AutoSaveChanges { get; set; }
+
+	Task<TEntity> AddAsync(TEntity entity, CancellationToken token = default);
+	Task<TEntity> UpdateAsync(TEntity entity, CancellationToken token = default);
+	Task<TEntity> RemoveAsync(TEntity entity, CancellationToken token = default);
+}
