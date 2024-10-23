e2e1a05e7e89f0b1b89bbd90c6cc3ac4c3ec0d5a
Emilio <emiliogg84@outlook.com>
2024-10-23 10:14:30 -0500

    Create EntityCode.cs

---
+++ src/Sumodesk.Domain/Abstractions/EntityCode.cs
@@ -0,0 +1,8 @@
+﻿using Sumodesk.Domain.Interfaces;
+
+namespace Sumodesk.Domain.Abstractions;
+
+public abstract class EntityCode : Entity, IEntityCode
+{
+    public required string Code { get; set; }
+}
