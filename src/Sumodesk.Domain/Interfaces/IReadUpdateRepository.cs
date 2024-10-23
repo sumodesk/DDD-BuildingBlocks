be955f6a46e7bdc8f61af1e05eb483e206f66018
Emilio <emiliogg84@outlook.com>
2024-10-23 13:32:24 -0500

    Create IReadUpdateRepository.cs

---
+++ src/Sumodesk.Domain/Interfaces/IReadUpdateRepository.cs
@@ -0,0 +1,10 @@
+﻿using Sumodesk.Domain.Abstractions;
+
+namespace Sumodesk.Domain.Interfaces;
+
+public interface IReadUpdateRepository<TEntity>
+	where TEntity : class, IEntityRoot
+{
+	Task<TEntity> GetByIdAsync(int id, IncludeProps<TEntity>? includeProps = default, CancellationToken token = default);
+	Task<TEntity> UpdateAsync(TEntity entity, CancellationToken token = default);
+}
